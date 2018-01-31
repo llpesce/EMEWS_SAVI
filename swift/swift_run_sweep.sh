bcc598113832ee1145b850ab0559936a81b06358
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-01-31 15:30:11 -0600

    Use sync_exec in hook definition

--- swift/swift_run_sweep.sh
+++ swift/swift_run_sweep.sh
@@ -66,7 +66,11 @@ log_script
 
 #Introduce the two hooks that allow swift to run one preparation and clean up function per node
 export TURBINE_LEADER_HOOK_STARTUP=" puts [ exec ${EMEWS_PROJECT_ROOT}/scripts/hook-startup.sh ]"
-#export TURBINE_LEADER_HOOK_SHUTDOWN=" puts [ exec ${EMEWS_PROJECT_ROOT}/scripts/hook-shutdown.sh ]"
+# export TURBINE_LEADER_HOOK_SHUTDOWN=" puts [ exec ${EMEWS_PROJECT_ROOT}/scripts/hook-shutdown.sh ]"
+
+export TURBINE_LEADER_HOOK_SHUTDOWN="c::sync_exec {} {} {} ${EMEWS_PROJECT_ROOT}/scripts/hook-shutdown.sh"
+
+# export TURBINE_LEADER_HOOK_SHUTDOWN="puts SHUTDOWN"
 
 #write out what is being used.
 echo "Swift/T PATH entries:"
